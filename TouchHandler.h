012b18aa6a90459d4eeb725f34778d7f951e9e8a
Michał Kiełtyka <defozo@gmail.com>
2023-04-10 23:44:53 +0200

    Initial commit

---
+++ TouchHandler.h
@@ -0,0 +1,30 @@
+#ifndef TouchHandler_h
+#define TouchHandler_h
+
+#include <Arduino.h>
+
+class TouchHandler {
+public:
+  TouchHandler(const int* touchPins, int numPins, int samplePeriod = 10, int initPeriod = 5000, int filterPeriod = 10000, float factor = 4.0, float offset = 0.0);
+  void begin();
+  bool isTouched(int index);
+  int getTouchValue(int index);
+  float getThreshold(int index);
+  void update();
+
+private:
+  const int* _touchPins;
+  int _numPins;
+  int _samplePeriod;
+  int _initPeriod;
+  int _filterPeriod;
+  float _factor;
+  float _offset;
+  float* _medians;
+  float* _touchStdDevs;
+  uint32_t _start_time;
+  float* _epsilon_m;
+  float* _epsilon_s;
+};
+
+#endif
